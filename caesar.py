b0f7e26982d5abbcb75beefa9900e6efcdd496b5
Patrick Lippert <patrick.lippert1@gmail.com>
2017-06-21 11:39:33 -0700

    Added to caesar.py

--- caesar.py
+++ caesar.py
@@ -16,3 +16,24 @@ def rotate_string_13(text):
             rotated = rotated + alphabet[rotated_idx]
 
     return rotated
+
+def rotate_character(char, rot):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    rotated_idx = (alphabet_position(char) + rot) % 26
+
+    if char.isupper():
+        return alphabet[rotated_idx].upper()
+    else:
+        return alphabet[rotated_idx]
+
+def rotate_string(text, rot):
+
+    rotated = ''
+
+    for char in text:
+        if (char.isalpha()):
+            rotated = rotated + rotate_character(char, rot)
+        else:
+            rotated = rotated + char
+
+return rotated
