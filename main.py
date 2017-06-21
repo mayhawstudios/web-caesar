153a2d8314e2e07c965f9153740003e901a59774
Patrick Lippert <patrick.lippert1@gmail.com>
2017-06-21 13:55:16 -0700

    Added error handling and run caesar cypher

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from caesar import rotate_string
+import cgi
 
 app = Flask(__name__)
 
@@ -11,29 +12,31 @@ page_header = """
 <html>
     <head>
         <style>
-            form {{
+            form {
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }}
-            textarea {{
+            }
+            textarea {
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }}
+            }
         </style>
     </head>
     <body>
  """
 cypher_form = """
-<form action="/cypher" method="POST">
+<form action="/encrypt" method="POST">
     Rotate By :<input type="text" name="rot" id="rot" value="0"><br>
-    <textarea name="text">{0}</textarea><br>
+    <textarea name="text">
+    </textarea><br>
     <input type="submit" value="Submit Query">
 </form>
+<p name="error"></p>
 """
 
 page_footer = """
@@ -43,10 +46,33 @@ page_footer = """
 
 @app.route("/encrypt", methods=['POST'])
 def encrypt():
-    return False
+    rot = request.form['rot']
+    plaintext = request.form['text'].strip()
+
+    if type(int(rot)) != int:
+        error = "You must input a valid number."
+
+        return redirect("/?error=" + error)
+
+    if plaintext.strip() == "":
+        error = "You cannot leave text blank."
+
+        return redirect("/?error=" + error)
+    else:
+        rot = int(rot)
+
+        return rotate_string(plaintext,rot)
 
 @app.route("/")
 def index():
+    # if we have an error, make a <p> to display it
+    error = request.args.get("error")
+    if error:
+        error_esc = cgi.escape(error, quote=True)
+        error_element = '<p>' + error_esc + '</p>'
+    else:
+        error_element = ''
+
     form = page_header + cypher_form + page_footer
     return form
 
